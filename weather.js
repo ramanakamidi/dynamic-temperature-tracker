3da8535dbface2fbb7cccbe6b803ee729a91c945
Ramana81 <144616152+Ramana81@users.noreply.github.com>
2024-04-11 20:14:49 +0530

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,20 @@
+input=document.getElementById('inputfield');
+btn=document.querySelector('#btn');
+city=document.querySelector('#city');
+temp=document.querySelector('#celsius');
+wind=document.querySelector('#windspeed');
+fetcher=document.querySelector('.apifields');
+
+async function handleTemperature(){
+    var response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input.value}&appid=4c71d9d0da5dcf3b33ce2c631c33f61f&units=metric`);
+    var data= await response.json();
+    console.log(data);
+    fetcher.style.display = 'block';
+    city.innerHTML=`your city is : ${data.name}`;
+    temp.innerHTML=`temperature :${data.main.temp}degrees`;
+    wind.innerHTML=`wind speed :${data.wind.speed}km/hr`;
+
+
+}
+btn.addEventListener('click', handleTemperature);
+
